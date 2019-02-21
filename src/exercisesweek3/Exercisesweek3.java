b0fdf172dfb5feca89166c7107517d55aeb3bbe0
16001551 <16001551@PC-004570.uhi.ad.local>
2019-02-21 14:08:40 +0000

    Working week 3

--- src/exercisesweek3/Exercisesweek3.java
+++ src/exercisesweek3/Exercisesweek3.java
@@ -17,17 +17,19 @@ public class Exercisesweek3 {
         int choice;
                 Scanner stdin = new Scanner(System.in);
                 
-                int numbers[] = {34,67,2,4,89,1,3,78};
+                int numbers[] = {2,3,4,5};
           
                 
 
         ArrayOfNumbers n1 = new ArrayOfNumbers(numbers);
-
-
+      
 
 
         System.out.println("Welcome to the Tally Counter application");
-        System.out.println("1 = Get Number, 2 = First Number, 3 = Find the biggest Number, 4 = Are they equal? 0 = Exit");
+        System.out.println("1 = Get Number, 2 = First Number, 3 = Find the biggest Number, 4 = Are they equal?, 5 = add new numbers, 6 = display all 0 = Exit");
+ 
+        
+        
         do {
             System.out.print("Next action: ");
             choice = stdin.nextInt();
@@ -40,10 +42,11 @@ public class Exercisesweek3 {
             } else if (choice == 4) {
                 n1.equal();
              } else if (choice == 5) {
-                n1.GCD();
+                //n1.GCD();
                 n1.addnumbers();
                 } else if (choice == 6) {
                 n1.toString();
+                 n1.display();
                 }
              else if (choice != 0) {
                 System.out.println("Error: Valid choices are 0, 1 or 2");
