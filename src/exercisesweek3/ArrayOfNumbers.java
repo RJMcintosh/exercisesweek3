fa63141682e8cea63cf96a6a7679654ad5b9c83b
16001551 <16001551@PC-004570.uhi.ad.local>
2019-02-21 14:24:49 +0000

    Working week 3 - update

--- src/exercisesweek3/ArrayOfNumbers.java
+++ src/exercisesweek3/ArrayOfNumbers.java
@@ -5,6 +5,7 @@
  */
 package exercisesweek3;
 
+import java.util.Random;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -20,73 +21,92 @@ public class ArrayOfNumbers {
     private int GCD;
     private String string;
     
-   
-    public void GetNumber(int num1){
-
-    }
- 
-    public void first() {
-        System.out.println(number1);
+      public ArrayOfNumbers(int number){
+    this.numbers = new int[number];
     }
     
-    public void second() {
-       System.out.println(number2);
+   public ArrayOfNumbers(int[] array){
+   this.numbers = new int[array.length];
+   int size = array.length;
+   for (int i=0; i<array.length;i++){
+   numbers[i]= array[i];
+   }
+   System.out.println("Array Information copied to storage\n");
+   System.out.println(Arrays.toString(numbers));
+   System.out.println("Size"+size);
+   }
+ 
+    public int first() {
+        System.out.println(numbers[0]);
+        return numbers[0];
+    }
+    
+    public int second() {
+       System.out.println(numbers[1]);
+       return numbers[1];
     }
     
     public void max(){
-    for(int i=0;i!=1;i++){
-           if(number1 > number2){
-               max = number1;
-	  System.out.println(number1);
+       int max = numbers[0];
+    for(int i=1;i<numbers.length;i++){
+           if( numbers[i]>max){
+               max = numbers[i];
+
+	}
           
-	}else{
-               max = number2;
-          System.out.println(number2);  
-           }
-        }
+        } System.out.println("Max number is "+max);
     }
     
     public void equal(){
-         if(number1==number2){
+        for(int i=0;i!=numbers.length;i++){
+         if(numbers[0]==numbers[i]){
         System.out.println("True");
         equal = true;
        }else{
         System.out.println("False");
         equal = false;
+         }
         }
     }
     
     
     public void  GCD() {
-        if(number2 == 0){
-            System.out.println(number1);
-        }
-        int number;
-        number = number1%number2;
-          System.out.println(number);
+        Random random = new Random();
+        int numb1 = random.nextInt(numbers.length);
+        int numb2;
+        do{    
+               numb2 = random.nextInt(numbers.length);  
+        }while(numb1==numb2);
+      
+        int number = numbers[numb1];
+         int number2 = numbers[numb2];
+        int gcd = number%number2;
+          System.out.println("GCD = "+gcd);
     }
     
    
     
     public void addnumbers(){
                     Scanner stdin = new Scanner(System.in);
-                
-         System.out.print("Enter another number: ");
-            number3 = stdin.nextInt();
-            System.out.print("Enter another number: ");
-            number4 = stdin.nextInt();
-        
-    number1 = number1+number3;
-    number2 = number2+number4; 
-    System.out.println("New number 1 is "+number1+ " new number 2 is "+number2);
+              
+           for(int i=0;i!=numbers.length;i++){
+              int n=i;
+    
+                System.out.print("Enter another number: ");
+                n = stdin.nextInt();
+               
+                numbers[i] = numbers[i]+n;
+            
+            System.out.println("New number "+n+" is "+numbers[i]);
+        }
     }
     
     
     @Override
     public String toString(){
         
-       string = ("First number = "+number1 +
-        " Second number = "+number2+
+       string = ("First number = "+numbers[0] +
+        " Second number = "+numbers[1]+
           " Max = "+max+
          " First number = "+equal+
             " First number = "+GCD);
@@ -99,18 +119,5 @@ public class ArrayOfNumbers {
     
     }
      
-    private ArrayOfNumbers(int value1){
-    this.numbers = new int[value1];
-    }
-    
-   public ArrayOfNumbers(int[] array){
-   this.numbers = new int[array.length];
-   int size = numbers.length;
-   for (int i=0; 1<array.length;i++){
-   numbers[i]= array[i];
-   }
-   System.out.println("Array Information copied to storage\n");
-   System.out.println(Arrays.toString(numbers));
-   System.out.println("Size"+size);
-   }
+  
 }
